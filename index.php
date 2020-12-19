8f79553c56829aa02e2e7b477f41cbd2a7362050
Cassandre <cassandre.michelet@gmail.com>
2020-12-19 13:58:18 +0100

    New post (connect to bdd + echo new post accueil)

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php 
+// Connexion à la bdd
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=oc_p4;charset=utf8', 'root', '');
+}
+catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+}
+$reponses = $bdd->query('SELECT titre, contenu, DATE_FORMAT(post_date, \'%d/%m/%Y %Hh%imin%ss\') AS post_date FROM billets ORDER BY id DESC LIMIT 0,5');
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,18 +21,29 @@
         <!-- MENU -->
         <header>
             <button id="boutonConnect">Connexion</button>
-            <!-- <form method="post" action="php/connectForm.php" id="connectForm">
+            <form method="post" action="php/connectForm.php" id="connectForm">
                     <label for="pseudo">Pseudo: </label><input type="text" name="pseudo"/><br>
                     <label for="motdepasse">Mot de passe: </label><input type="password" name="motdepasse"/><br>
                     <input type="submit" value="Se connecter"/>
-            </form> -->
-            <img src="images/Alaska-7.png" alt="triptyque montagnes">
+            </form>
+            <img id="montagnes" src="images/montagnes.png" alt="montagnes billet simple pour l'Alaska">
         </header>
 
         <!-- BILLETS -->
         <section id='sectionBillets'>
+            <!-- Récupération des 5 derniers billets postés -->
+            <?php 
+            while($donnees = $reponses->fetch()) {
+            ?>
+            <div class='new_billet'>
+                <h3><?php echo htmlspecialchars($donnees['titre']) .' - '. htmlspecialchars($donnees['post_date']); ?></h3>
+                <p><?php echo htmlspecialchars($donnees['contenu']); ?></p>
+            </div>
+            <?php
+            }
+            $reponses->closeCursor();?>
         </section>
     </body>
 
-    <script src="js/connect.js"></script>
+    <!-- <script src=".js"></script> -->
 </html>
--- php/admin.php
+++ php/admin.php
@@ -1,31 +1,43 @@
+<?php 
+// Connexion à la bdd
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=oc_p4;charset=utf8', 'root', '');
+}
+catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
         <title>Admin</title>
-        <!-- <link rel="stylesheet" media="screen" href="css/accueil.css"> -->
+        <link rel="stylesheet" media="screen" href="../css/admin.css">
     </head>
     
     <body>
-        <!-- GEADER -->
+        <!-- HEADER -->
         <header>
-            <h1>admin <strong><?= $_POST['pseudo']?></strong></h1>
-            <nav>
-                <ul>
-                    <li>Mes billets</li>
-                    <li>Brouillons</li>
-                    <li>Écrire un nouveau billet</li>
-                </ul>
-            </nav>
+            <img id="montagnes_admin" src="../images/admin.png" alt="montagnes admin">
+            <h1>Bienvenue <strong><?= $_POST['pseudo']?></strong></h1>
+            <div id="menu_admin">
+                <nav>
+                    <ul>
+                        <li>Mes billets</li>
+                        <li>Brouillons</li>
+                        <li>Écrire un nouveau billet</li>
+                    </ul>
+                </nav>
+            </div>
         </header>
 
         <!-- NOUVEAU BILLETS -->
         <section id='newBillet'>
             <h2>Nouveau Billet</h2>
-            <form action="">
+            <form method='post' action="new_billet.php">
             <label for="titre">Titre: </label><input type="text" name="titre" autofocus/><br>
-            <label for="date">Date: </label><input type="date" name="date"/><br>
-            <textarea name="newChapter" id="newChapter" cols="150" rows="30"></textarea><br>
+            <label for="post_date">Date: </label><input type="date" name="post_date"/><br>
+            <textarea name="contenu" id="newChapter" cols="137" rows="30"></textarea><br>
             <input type="button" value="Sauvegarder"/>
             <input type="submit" value="Publier"/>
             </form>
