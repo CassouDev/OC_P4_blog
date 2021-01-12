a3907e74782dcc31be759ccdb7b4b7013533795f
Cassandre <cassandre.michelet@gmail.com>
2021-01-12 16:45:00 +0100

    Tabs in admin + design admin & billets

--- php/admin.php
+++ php/admin.php
@@ -6,6 +6,7 @@ try {
 catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
 }
+$reponses = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(post_date, \'%d/%m/%Y %Hh%imin%ss\') AS post_date FROM billets ORDER BY id DESC LIMIT 0,5');
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,6 +15,7 @@ catch (Exception $e) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Admin</title>
         <link rel="stylesheet" media="screen" href="../css/admin.css">
+        <link rel="stylesheet" media="screen" href="../css/tabs.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     </head>
     
@@ -22,50 +24,42 @@ catch (Exception $e) {
         <header>
             <img id="montagnes_admin" src="../images/admin.png" alt="montagnes admin">
             <h1>Bienvenue <strong><?= $_POST['pseudo']?></strong></h1>
-            <div id="menu_admin">
-                <nav>
-                    <ul>
-                        <li>Mes billets</li>
-                        <li>Brouillons</li>
-                        <li>Écrire un nouveau billet</li>
-                    </ul>
-                </nav>
-            </div>
         </header>
 
-        <!-- NOUVEAU BILLETS -->
-        <div class="container">
-            <div class="row" id='newTitle'>
-                <h2>Nouveau Billet</h2>
+        <div id="tabs">
+            <div id="tabMenu">
+                <div class="tab whiteborder">Nouveau billet</div>
+                <div class="tab">Mes billets</div>
             </div>
-            <div class="row" id='newBillet'>
-                <form method='post' action="new_billet.php">
-                <label for="titre">Titre: </label><input type="text" name="titre" autofocus/><br>
-                <label for="post_date">Date: </label><input type="date" name="post_date"/><br>
-                <textarea name="contenu" id="newChapter" cols="137" rows="30"></textarea><br>
-                <input type="button" value="Sauvegarder"/>
-                <input type="submit" value="Publier"/>
-                </form>
+            <div class="tabContent">
+                <div id="newBillet">
+                    <form method='post' action="new_billet.php">
+                    <label id="titreBillet" for="titre">Titre:</label><input id="inputTitre" type="text" name="titre" autofocus/><br>
+                    <label id="dateBillet" for="post_date">Date:</label><input id="inputDate" type="date" name="post_date"/><br>
+                    <textarea name="contenu" cols="70" rows="20"></textarea><br>
+                    <input id="boutonPublier" type="submit" value="Publier"/>
+                    </form>
+                </div>
+            </div>
+            <div class="tabContent">
+                <section id='sectionBillets'>
+                    <!-- Récupération des 5 derniers billets postés -->
+                    <?php 
+                    while($donnees = $reponses->fetch()) {
+                    ?>
+                    <a href="php/billets.php?idBillet=<?php echo $donnees['id']; ?>">
+                        <div class='derniersBillets'>
+                            <h3><?php echo htmlspecialchars($donnees['titre'])?></h3>
+                        </div>
+                    </a>
+                    <?php
+                    }
+                    $reponses->closeCursor();?>
+                </section>
             </div>
         </div>
-
-            <!-- MES BILLETS -->
-            <section id='new'>
-                <h2>Mes BILLETS</h2>
-            </section>
-        </div>
-
-    <!-- Optional JavaScript -->
-
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-       
+            
     </body>
 
-    <!-- <script src=""></script> -->
+    <script src="../js/tabs.js"></script>
 </html>
